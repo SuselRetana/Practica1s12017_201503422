95313c40328b6f2cdb5a0b02bee72d680b00fd61
Susel <201503422@ingenieria.usac.edu.gt>
2017-02-17 10:29:43 -0600

    Creacion Proyecto
    creacion proyecto en netbeans 8.2

---
+++ [EDD]Practica1_201503422/src/edd/practica1_201503422/EDDPractica1_201503422.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd.practica1_201503422;
+
+/**
+ *
+ * @author freni_000
+ */
+public class EDDPractica1_201503422 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
